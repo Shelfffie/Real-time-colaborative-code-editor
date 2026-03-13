a5c986e15d62313164fb90d27f6d16feb41da8a1
Polina <zilnovapolina@gmail.com>
2026-03-13 17:30:17 +0200

    feat: write saveChanges func in backend that will be saving changes to db and create version of code in changes db

--- backend/src/controllers/code_controller.ts
+++ backend/src/controllers/code_controller.ts
@@ -20,7 +20,11 @@ const createCodeSession = async (
     const insertQuery =
       "INSERT INTO sessions (title, content) VALUES ($1, $2) RETURNING *";
     const result = await db.query(insertQuery, [title, content]);
-    return responseHandler(res, 200, true, result.rows[0]);
+    const sessionInfo = result.rows[0];
+    const copyQuery =
+      "INSERT INTO changes_history (session_id, title, content, version) VALUES($1, $2, $3)";
+    await db.query(copyQuery, [sessionInfo.id, title, content, 1]);
+    return responseHandler(res, 200, true, sessionInfo);
   } catch (error: unknown) {
     errorCatchHandler(error, res);
   }
@@ -49,4 +53,30 @@ const getCodeSession = async (
   }
 };
 
-export { createCodeSession, getCodeSession };
+const saveChanges = async (
+  req: Request<{ id: string }>,
+  res: Response<ApiResponse<SessionType>>
+) => {
+  const id: number = parseInt(req.params.id);
+  const { content } = req.body;
+
+  if (isNaN(id) || id <= 0) {
+    return responseHandler(res, 401, false, "Invalid id.");
+  }
+  try {
+    await db.query("BEGIN");
+    const putQuery = "UPDATE sessions SET content=$1 WHERE id=$2 RETURNING *";
+    const data = await db.query(putQuery, [content, id]);
+    const session = data.rows[0];
+    const copyQuery =
+      "INSERT INTO changes_history (session_id, title, content, version) VALUES($1, $2, $3, COALESCE((SELECT MAX(version) FROM changes_history WHERE session_id=$1), 0) + 1 )";
+    await db.query(copyQuery, [session.id, session.title, content]);
+    await db.query("COMMIT");
+    return responseHandler(res, 200, true, session);
+  } catch (error) {
+    await db.query("ROLLBACK");
+    errorCatchHandler(error, res);
+  }
+};
+
+export { createCodeSession, getCodeSession, saveChanges };
